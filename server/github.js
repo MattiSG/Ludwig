4add4e3e98b45179d453beb2776733baaface2ee
Matti Schneider <hi@mattischneider.fr>
2015-10-10 21:57:56 +0200

    Refactor GitHub API communication

--- server/github.js
+++ server/github.js
@@ -1,4 +1,9 @@
-const BASEPATH = 'https://api.github.com';
+const BASEPATH = 'https://api.github.com/repos',
+	headers = {
+		Accept: 'application/vnd.github.v3+json',
+		Authorization: `token ${Meteor.settings.oauth}`,
+		'User-Agent': 'Ludwig',
+	};
 
 Meteor.methods({
 	createPullRequest: function(content) {
@@ -6,49 +11,33 @@ Meteor.methods({
 			repo = 'loulou',
 			path = 'test-2.yaml',
 			target = 'master',
-			branch = 'ludwig-test-6';
+			branch = 'ludwig-test-7';
 
 		try {
-			let targetRef = HTTP.get(`${BASEPATH}/repos/${owner}/${repo}/git/refs/heads/${target}`, {
-				headers: {
-					Accept: 'application/vnd.github.v3+json',
-					Authorization: `token ${Meteor.settings.oauth}`,
-					'User-Agent': 'Ludwig',
-				}
+			let targetRef = HTTP.get(`${BASEPATH}/${owner}/${repo}/git/refs/heads/${target}`, {
+				headers
 			});
 
-			HTTP.post(`${BASEPATH}/repos/${owner}/${repo}/git/refs`, {
-				headers: {
-					Accept: 'application/vnd.github.v3+json',
-					Authorization: `token ${Meteor.settings.oauth}`,
-					'User-Agent': 'Ludwig',
-				},
+			HTTP.post(`${BASEPATH}/${owner}/${repo}/git/refs`, {
+				headers,
 				data: {
 					ref:	`refs/heads/${branch}`,
 					sha:	targetRef.data.object.sha,
 				}
 			});
 
-			HTTP.put(`${BASEPATH}/repos/${owner}/${repo}/contents/${path}`, {
-				headers: {
-					Accept: 'application/vnd.github.v3+json',
-					Authorization: `token ${Meteor.settings.oauth}`,
-					'User-Agent': 'Ludwig',
-				},
+			HTTP.put(`${BASEPATH}/${owner}/${repo}/contents/${path}`, {
+				headers,
 				data: {
-					path:		path,
-					branch:		branch,
+					path,
+					branch,
 					message:	'Add test',
 					content:	CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(content)),
 				}
 			});
 
-			HTTP.post(`${BASEPATH}/repos/${owner}/${repo}/pulls`, {
-				headers: {
-					Accept: 'application/vnd.github.v3+json',
-					Authorization: `token ${Meteor.settings.oauth}`,
-					'User-Agent': 'Ludwig',
-				},
+			HTTP.post(`${BASEPATH}/${owner}/${repo}/pulls`, {
+				headers,
 				data: {
 					base:	target,
 					head:	branch,
