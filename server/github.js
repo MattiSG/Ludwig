4715a91c2715350914d1aff53fd4879794114dfe
Matti Schneider <hi@mattischneider.fr>
2015-10-10 17:06:39 +0200

    Add create pull request method

--- server/github.js
+++ server/github.js
@@ -1,16 +1,23 @@
 var GithubApi = Meteor.npmRequire('github');
 var github = new GithubApi({
-    version: "3.0.0"
+	version: "3.0.0"
+});
+
+github.authenticate({
+	type:	'oauth',
+	token:	'7bc1698f98cbddb0c6781e1bce647fd654746ac4'
 });
 
 Meteor.methods({
-  'getGists': function getGists(user) {
-    var gists = Async.runSync(function(done) {
-      github.gists.getFromUser({user: 'arunoda'}, function(err, data) {
-        done(null, data);
-      });
-    });
-
-    return gists.result;
-  }
+	createPullRequest: () => {
+		return Async.runSync((done) => {
+			github.pullRequests.create({
+				user:	'Flightan',
+				repo:	'loulou',
+				base:	'master',
+				head:	'ludwig-test',
+				title:	'Test PR',
+			}, done);
+		}).result;
+	}
 });
